10aca50220cc13fa6a4cefebb6623981ba103081
oleast <oleast@stud.ntnu.no>
2020-05-15 11:52:37 +0200

    Create add tags dialog

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import { TagButton } from './TagButton';
+import React, { useState } from 'react';
 
-export const Tags = ({ tags }) => {
+import { useUser } from 'common/hooks/userUser';
+
+import { TagButton } from './TagButton';
+import { useRouter } from 'next/router';
+import { AddTagDialog } from './AddTagDialog';
+
+export const Tags = ({ tags, courseCode, reloadTags }) => {
+  const [user] = useUser();
+  const router = useRouter();
+  const [showDialog, setShowDialog] = useState(false);
+
+  const openDialog = () => setShowDialog(true);
+  const closeDialog = () => setShowDialog(false);
+
+  const handleClick = () => {
+    if (!user) {
+      router.push({ pathname: '/login', query: { returnToPath: router.asPath } });
+    } else {
+      openDialog();
+    }
+  };
+
   return (
     <div className="well">
       <h4>Tags</h4>
@@ -14,6 +34,17 @@ export const Tags = ({ tags }) => {
           </div>
         </li>
       </ul>
+      <button type="button" className="btn btn-default" onClick={handleClick}>
+        <span className="glyphicon glyphicon-plus" aria-hidden="true" />
+        Legg til
+      </button>
+      {!user && (
+        <>
+          <small className="text-muted">Du må være logget inn for å kunne legge til tags.</small>
+          <br />
+        </>
+      )}
+      <AddTagDialog isOpen={showDialog} closeDialog={closeDialog} courseCode={courseCode} reloadTags={reloadTags} />
     </div>
   );
 };
