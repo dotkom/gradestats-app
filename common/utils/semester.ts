2d0b792f5b7e5ffec5e0fcbf15baad1dd74897f5
oleast <ole.stokker@bekk.no>
2021-01-02 01:17:13 +0100

    chore(typescript): typescriptify semester sort

---
+++ common/utils/semester.ts
@@ -0,0 +1,11 @@
+import { Grade, Semester } from 'models/Grade';
+
+const SEMESTERS: Semester[] = ['SPRING', 'SUMMER', 'AUTUMN'];
+
+export const sortSemesters = (gradeA: Grade, gradeB: Grade) => {
+  if (gradeA.year === gradeB.year) {
+    return SEMESTERS.indexOf(gradeA.semester) - SEMESTERS.indexOf(gradeB.semester);
+  } else {
+    return gradeA.year - gradeB.year;
+  }
+};
--- models/Grade.ts
+++ models/Grade.ts
@@ -1,11 +1,14 @@
+export type Semester = 'SPRING' | 'SUMMER' | 'AUTUMN';
+
 export interface Grade {
   id: number;
   course: number;
+  semester: Semester;
   semester_display: string;
   year: number;
   semester_code: string;
   average_grade: number;
-  digital_exam: true;
+  digital_exam: boolean;
   passed: number;
   a: number;
   b: number;
