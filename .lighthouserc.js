67942015b5cc1e72dbcae25dfd37fdfad4142bbd
oleast <ole.stokker@bekk.no>
2021-01-07 20:26:04 +0100

    chore(ci): turn off breaking rules in lighthouse CI

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -15,5 +15,26 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'first-contentful-paint': 'off', // Ouff performance
+        'first-meaningful-paint': 'off', // Ouff performance
+        'largest-contentful-paint': 'off', // Ouff performance
+        interactive: ['error', { minScore: 0.65 }], // Ouff performance
+        'max-potential-fid': ['error', { minScore: 0.5 }], // Ouff performance
+        'first-cpu-idle': ['error', { minScore: 0.7 }], // Ouff performance
+        'legacy-javascript': 'off', // Maybe Google Analytics?
+        'uses-rel-preload': 'off', // Maybe fixable?
+        'unused-javascript': 'off', // Maybe fixable?
+        'works-offline': 'off', // Enable for PWA
+        'offline-start-url': 'off', // Enable for PWA
+        'service-worker': 'off', // Enable for PWA
+        'content-width': 'off', // Should be fixed
+        'tap-targets': 'off', // Should be fixed
+        'unsized-images': 'off', // Off because Next Image handles it better?
+        'uses-text-compression': 'off', // Handled by Nginx and such
+      },
+    },
   },
 };
