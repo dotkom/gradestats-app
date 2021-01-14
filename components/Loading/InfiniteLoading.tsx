ef5cd361a7697b7818ed7f2005ab94cdf24f4a19
oleast <ole.stokker@bekk.no>
2021-01-14 23:05:54 +0100

    fix(course-list): align table and split out infinite loading icon

---
+++ components/Loading/InfiniteLoading.tsx
@@ -0,0 +1,26 @@
+import { useIntersection } from 'common/hooks/useIntersection';
+import { AnimatedGraphIcon } from 'components/Graphics/AnimatedGraphIcon';
+import { Text } from 'components/Typography/Text';
+import React, { FC } from 'react';
+
+import styles from './infinite-loading.module.scss';
+
+interface Props {
+  isLoading: boolean;
+  triggerNextPage: () => void;
+}
+
+export const InifiniteLoading: FC<Props> = ({ isLoading, triggerNextPage }) => {
+  const ref = useIntersection(triggerNextPage);
+  return (
+    <>
+      {isLoading && (
+        <div className={styles.loadingContainer}>
+          <AnimatedGraphIcon className={styles.loadingIcon} />
+          <Text>Laster...</Text>
+        </div>
+      )}
+      <span ref={ref} />
+    </>
+  );
+};
