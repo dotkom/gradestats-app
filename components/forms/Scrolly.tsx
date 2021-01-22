efb0818dfada44d2d271e0089e0ea3e40971415e
oleast <ole.stokker@bekk.no>
2021-01-22 22:20:49 +0100

    fix(scrolly): scroll to first element when semester filter changes

--- components/forms/Scrolly.tsx
+++ components/forms/Scrolly.tsx
@@ -1,8 +1,9 @@
 import { Button } from 'components/common/Button';
-import React, { FC, MouseEvent, useRef } from 'react';
+import React, { createRef, FC, MouseEvent, useMemo, useRef } from 'react';
 import cx from 'classnames';
 
 import styles from './scrolly.module.scss';
+import { useIsomorphicLayoutEffect } from 'common/hooks/useIsomorphicLayoutEffect';
 
 interface Props {
   className?: string;
@@ -13,25 +14,39 @@ interface Props {
 
 export const Scrolly: FC<Props> = ({ className, selectedValue, values, onClick }) => {
   const scrollRef = useRef<HTMLDivElement>(null);
+  const refs = useMemo(() => {
+    return Object.fromEntries(values.map((value) => [value, createRef<HTMLButtonElement>()]));
+  }, [String(values)]);
+
+  const scrollButtonToMiddle = (button: HTMLButtonElement) => {
+    const scrolly = scrollRef.current;
+    if (scrolly) {
+      const { width: scrollyWidth } = scrolly.getBoundingClientRect();
+      const { width: buttonWidth } = button.getBoundingClientRect();
+      const middlePosition = button.offsetLeft - scrollyWidth / 2 + buttonWidth / 2;
+      scrolly.scrollTo({ behavior: 'smooth', left: middlePosition });
+    }
+  };
 
   const handleClick = (value: string) => (event: MouseEvent<HTMLButtonElement>) => {
     onClick(value);
     const clickedElement = event.target as HTMLButtonElement;
-    const scrolly = scrollRef.current;
-    if (scrolly) {
-      const { width: scrollyWidth } = scrolly.getBoundingClientRect();
-      const button = event.target as HTMLButtonElement;
-      const { width: buttonWidth } = button.getBoundingClientRect();
-      const middlePosition = clickedElement.offsetLeft - scrollyWidth / 2 + buttonWidth / 2;
-      scrolly.scrollTo({ behavior: 'smooth', left: middlePosition });
-    }
+    scrollButtonToMiddle(clickedElement);
   };
+
+  useIsomorphicLayoutEffect(() => {
+    const button = refs[selectedValue]?.current;
+    if (button) {
+      scrollButtonToMiddle(button);
+    }
+  }, [selectedValue, refs]);
 
   return (
     <div ref={scrollRef} className={cx(styles.scrollOuter, className)}>
       <div className={styles.scrollInner}>
         {[...values].reverse().map((value) => (
           <Button
+            ref={refs[value]}
             className={styles.scrollButton}
             key={value}
             onClick={handleClick(value)}
